20d7af118114216e7aff0283e59441f900fb1707
kw-codenation <kw-lea-codenation@gmx.co.uk>
2023-04-09 16:15:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import ToDoListItem from './ToDoListItem'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+    /*
+        Display a To Do list. This page will also allow to add an
+        item and delete one as well. This list also has a button 
+        thatwill display the status 'active' or 'done'. clicking
+        the button will change the status
+
+    */
+    const [list, setList] = useState([])
+
+    const message = 'There are no items in your list you lazy so and so'
+
+    function addItem(desc)
+    {
+        /*
+            Add and item to the to do list
+        */
+
+        setList(state => [...state, {desc:desc, status:'active'}])
+    }
+
+    function deleteItem(i)
+    {
+        /*
+            Delete an item from the to do list
+        */
+
+        setList((item) => item.filter((_, index) => index !== i))
+    }
+
+    function updateStatus(i)
+    {
+        /*
+            update the status on a particular item
+        */
+
+        /*let arr = [...list]*/
+        
+        (list[i].status === 'done') ? list[i].status = 'status' : list[i].status = 'done'
+
+        setList(list)
+        
+    }
+
+    return (
+        <div className="to-do-list">
+            <div className="to-do-list-title">To Do List</div>
+            {
+                (list.length === 0) ? message : list.map((item, index) => <ToDoListItem key={index} desc={item.desc} status={item.status} updateStatus={updateStatus} deleteItem={deleteItem} index={index} />)
+            }
+            <p>To add an item to the list, enter a description and click 'add'</p>
+            <input type="text" className="to-do-list-input" id="desc"></input>
+            <input type="button" className="to-do-list-add" value="add" onClick={() => addItem(document.getElementById('desc').value)} ></input>
+        </div>
+    );
 }
 
 export default App;
